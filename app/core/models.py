5bf21d0e456e5c25f8794467761a813f7bf3db36
Pradip Thapa <pradipthapaofficial@gamail.com>
2023-01-22 20:50:29 +0545

    Fixed lint.

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,6 @@
 """
 Database models.
 """
-from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,7 @@ Tests for models.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -35,7 +36,7 @@ class ModelTests(TestCase):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
-    
+
     def test_create_superuser(self):
         """Test creating a superuser."""
         user = get_user_model().objects.create_superuser(
