f9693530b3dac406a4c708486e076321639f08f0
Pradip Thapa <pradipthapaofficial@gamail.com>
2023-01-23 12:39:32 +0545

    Add test edit user page or up date core admin.py

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,16 +1,32 @@
 """
-Django Admin customization.
+Django admin customization.
 """
-
 from django.contrib import admin
-from .models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from core import models
 
 
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
 
 
-admin.site.register(User, UserAdmin)
+admin.site.register(models.User, UserAdmin)
