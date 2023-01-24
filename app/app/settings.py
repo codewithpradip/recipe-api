2570eac09afe6ef83a27487c800bad56aa8c45df
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-01-24 09:33:32 +0545

    Add API documentation.

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_spectacular',
     'core'
 ]
 
@@ -129,3 +131,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "core.User"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
