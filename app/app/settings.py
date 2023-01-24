6a22ce6354d3e654f1697ca0d387c0f8bcd0c19c
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-01-24 12:40:57 +0545

    Implement create user API.

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_spectacular',
-    'core'
+    'core',
+    'user'
 ]
 
 MIDDLEWARE = [
--- app/app/urls.py
+++ app/app/urls.py
@@ -20,7 +20,7 @@ from drf_spectacular.views import (
 )
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,5 +29,6 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
-    )
+    ),
+    path('api/user/', include('user.urls'))
 ]
