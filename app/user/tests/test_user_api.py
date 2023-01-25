3c946f139c6638df0b4ebfc49989fe3cafd3f95e
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-01-25 14:40:46 +0545

    Implement user manage API.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -112,12 +112,13 @@ class PublicUserApiTests(TestCase):
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_retrieve_use_unauthorized(self):
+    
+    def test_retrieve_user_unauthorized(self):
         """Test authentication is required for users."""
         res = self.client.get(ME_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
 
 class PrivateUserApiTests(TestCase):
     """Test API requests that require authentication."""
