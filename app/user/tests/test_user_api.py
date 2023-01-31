587f4bffa8321be6c66196f3bafa4c4e918640b1
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-01-31 14:34:03 +0545

    test: add create recipe test case.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -112,7 +112,7 @@ class PublicUserApiTests(TestCase):
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-    
+
     def test_retrieve_user_unauthorized(self):
         """Test authentication is required for users."""
         res = self.client.get(ME_URL)
