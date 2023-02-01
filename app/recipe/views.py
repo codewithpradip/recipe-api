7cb36d8021dc50bb7e45e2be566b0194f9b8b2be
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-02-01 13:38:25 +0545

    feat: implement recipe api endpoints.

---
+++ app/recipe/views.py
@@ -0,0 +1,25 @@
+"""
+Views for the recipe APIs
+"""
+
+from rest_framework import viewsets
+
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Recipe
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """ View for manage Recipe APIs. """
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """ Retrieve recipes for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
