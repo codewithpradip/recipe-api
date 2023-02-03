22737c7295c594796da4eed269ede0948bdb8b86
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-02-03 16:22:40 +0545

    feat: implement recipe detail end point.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -10,7 +10,7 @@ from core.models import Recipe
 from django.contrib.auth import get_user_model
 from recipe.serializers import (
     RecipeSerializer,
-    DetailRecipeSerializer,
+    RecipeDetailSerializer
 )
 
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -15,7 +15,7 @@ from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """ View for manage Recipe APIs. """
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -23,3 +23,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """ Retrieve recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+    def get_serializer_class(self):
+        """"Return the serializer class for the request."""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+        return self.serializer_class
