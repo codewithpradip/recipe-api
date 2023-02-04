b4b1ec24dc3d639e756ab2ca11ff398c3838de8b
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-02-04 19:23:18 +0545

    test: add full recipe update test.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -111,21 +111,46 @@ class PrivateRecipeApiTests(TestCase):
             self.assertEqual(getattr(recipe, k), v)
         self.assertEqual(recipe.user, self.user)
 
-        def test_partial_update ( self ) :
+        def test_partial_update(self):
             """Test partial update of a recipe."""
-            original_link = 'https://example.com/recipe.pdf'
-            recipe = create_recipe (
+            original_link = "https://example.com/recipe.pdf"
+            recipe = create_recipe(
                 user=self.user,
-                title='Sample recipe title',
+                title="Sample recipe title",
                 link=original_link,
             )
 
-            payload = {'title' : 'New recipe title'}
-            url = detail_url (recipe.id)
-            res = self.client.patch (url, payload)
+            payload = {"title": "New recipe title"}
+            url = detail_url(recipe.id)
+            res = self.client.patch(url, payload)
 
-            self.assertEqual (res.status_code, status.HTTP_200_OK)
-            recipe.refresh_from_db ( )
-            self.assertEqual (recipe.title, payload['title'])
-            self.assertEqual (recipe.link, original_link)
-            self.assertEqual (recipe.user, self.user)
+            self.assertEqual(res.status_code, status.HTTP_200_OK)
+            recipe.refresh_from_db()
+            self.assertEqual(recipe.title, payload["title"])
+            self.assertEqual(recipe.link, original_link)
+            self.assertEqual(recipe.user, self.user)
+
+        def test_full_update(self):
+            """Test full update of recipe."""
+            recipe = create_recipe(
+                user=self.user,
+                title="Sample recipe title",
+                link="https://exmaple.com/recipe.pdf",
+                description="Sample recipe description.",
+            )
+
+            payload = {
+                "title": "New recipe title",
+                "link": "https://example.com/new-recipe.pdf",
+                "description": "New recipe description",
+                "time_minutes": 10,
+                "price": Decimal("2.50"),
+            }
+            url = detail_url(recipe.id)
+            res = self.client.put(url, payload)
+
+            self.assertEqual(res.status_code, status.HTTP_200_OK)
+            recipe.refresh_from_db()
+            for k, v in payload.items():
+                self.assertEqual(getattr(recipe, k), v)
+            self.assertEqual(recipe.user, self.user)
