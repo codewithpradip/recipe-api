e6480fd4a25c879a5a0fe003ceffe40211615179
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-02-05 13:53:23 +0545

    test: Add test for create tag.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,6 +6,9 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from core import models
 
+def create_user(email='user@example.com', password='testpass123'):
+    """Create a return a new user."""
+    return get_user_model().objects.create_user(email, password)
 
 class ModelTests(TestCase):
     """Test models."""
@@ -64,3 +67,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
