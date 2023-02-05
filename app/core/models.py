9f2168ca19d342d8a6d0469ed478cc01f940e6fa
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-02-05 13:59:05 +0545

    feat: Add tag model.

--- app/core/models.py
+++ app/core/models.py
@@ -59,3 +59,15 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Tag(models.Model):
+        """Tag for filtering recipes."""
+
+        name = models.CharField(max_length=255)
+        user = models.ForeignKey(
+            settings.AUTH_USER_MODEL,
+            on_delete=models.CASCADE,
+        )
+
+        def __str__(self):
+            return self.name
