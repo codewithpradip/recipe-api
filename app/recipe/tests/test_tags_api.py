30daff71537c17650951712ea189d9c85a3eda69
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-02-09 17:54:26 +0545

    test(tags): add test cases to check authentication requirement for retrieving tags.

---
+++ app/recipe/tests/test_tags_api.py
@@ -0,0 +1,35 @@
+"""
+Tests for the tags API.
+"""
+from decimal import Decimal
+
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Tag
+
+from recipe.serializers import TagSerializer
+
+TAGS_URL = reverse('recipe:tag-list')
+
+
+def create_user(email='user@example.com', password='testpass123'):
+    """Create and return a user."""
+    return get_user_model().objects.create_user(email=email, password=password)
+
+
+class PublicTagsApiTests(TestCase):
+    """Test unauthenticated API requests."""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """Test auth is required for retrieving tags."""
+        res = self.client.get(TAGS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
