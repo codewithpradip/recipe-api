356fce6162fffe923e20a9bd17bac948edf78749
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-02-10 17:36:33 +0545

    test(tags): add test to update tag partial.

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -22,6 +22,11 @@ def create_user(email='user@example.com', password='testpass123'):
     return get_user_model().objects.create_user(email=email, password=password)
 
 
+def detail_url(tag_id):
+    """Create and return a tag detail url."""
+    return reverse("recipe:tag-detail", args=[tag_id])
+
+
 class PublicTagsApiTests(TestCase):
     """Test unauthenticated API requests."""
 
@@ -66,3 +71,15 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual (len (res.data), 1)
         self.assertEqual (res.data[0]['name'], tag.name)
         self.assertEqual (res.data[0]['id'], tag.id)
+
+    def test_update_tag(self):
+        """Test updating a tag."""
+        tag = Tag.objects.create(user=self.user, name='After Dinner')
+
+        payload = {'name': 'Dessert'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
