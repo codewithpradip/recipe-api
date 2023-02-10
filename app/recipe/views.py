e3fca51b5a45acd2afd1c0afd2107265d065cf52
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-02-10 17:49:14 +0545

    feat(tag): Implement delete tag API.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,7 +38,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
 
 class TagViewSet(
-    mixins.UpdateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
 ):
     """Manage tags in the database."""
 
