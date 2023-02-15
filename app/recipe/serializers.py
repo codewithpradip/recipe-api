7b8427804214b16163d40c7240703afeca0a9319
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-02-15 11:00:49 +0545

    feat(tags): implement update recipe tags.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -25,15 +25,15 @@ class RecipeSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ['id']
 
-    def _get_or_create_tags ( self, tags, recipe ) :
+    def _get_or_create_tags(self, tags, recipe):
         """Handle getting or creating tags as needed."""
-        auth_user = self.context['request'].user
-        for tag in tags :
-            tag_obj, created = Tag.objects.get_or_create (
+        auth_user = self.context["request"].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
                 user=auth_user,
                 **tag,
             )
-            recipe.tags.add (tag_obj)
+            recipe.tags.add(tag_obj)
 
     def create(self, validated_data):
         """Create a recipe."""
@@ -42,6 +42,19 @@ class RecipeSerializer(serializers.ModelSerializer):
         self._get_or_create_tags(tags, recipe)
 
         return recipe
+
+    def update(self, instance, validated_data):
+        """Update recipe."""
+        tags = validated_data.pop("tags", None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 class RecipeDetailSerializer(RecipeSerializer):
     """"Serializer for recipe detail view."""
