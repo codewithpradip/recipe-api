05ea04748579aa769d5b979f3941f433e9cbd605
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-02-16 19:42:19 +0545

    feat(ingredient): add ingredientviewset.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -53,3 +53,14 @@ class TagViewSet(
     def get_queryset(self):
         """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by("-name")
+
+class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage ingredients in the database."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
