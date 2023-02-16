ae7a9746d4d9f26863aa60da1c9486a40c03ac0d
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-02-16 19:33:16 +0545

    feat(ingredients): add ingredientserializer for serializing ingredients.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -2,8 +2,16 @@
 Serializers for recipe APIs
 """
 from rest_framework import serializers
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for ingredients."""
+
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name']
+        read_only_fields = ['id']
 
 class TagSerializer(serializers.ModelSerializer):
     """Serializer for tags."""
