e877a5cf47bc0281383c0e1289cd1aa5ae9bc60f
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-02-16 18:56:44 +0545

    test(ingredients): retrieving ingredients for auth user.

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -12,16 +12,15 @@ from rest_framework.test import APIClient
 from core.models import Ingredient
 from recipe.serializers import IngredientSerializer
 
-
-INGREDIENTS_URL = reverse('recipe:ingredient-list')
+INGREDIENTS_URL = reverse("recipe:ingredient-list")
 
 
 def detail_url(ingredient_id):
     """Create and return an ingredient detail URL."""
-    return reverse('recipe:ingredient-detail', args=[ingredient_id])
+    return reverse("recipe:ingredient-detail", args=[ingredient_id])
 
 
-def create_user(email='user@example.com', password='testpass123'):
+def create_user(email="user@example.com", password="testpass123"):
     """Create and return user."""
     return get_user_model().objects.create_user(email=email, password=password)
 
@@ -37,3 +36,24 @@ class PublicIngredientsApiTests(TestCase):
         res = self.client.get(INGREDIENTS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateIngredientsApiTests(TestCase):
+    """Test authenticated API requests."""
+
+    def setUp(self):
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_ingredients(self):
+        """Test retrieving a list of ingredients."""
+        Ingredient.objects.create(user=self.user, name="Kale")
+        Ingredient.objects.create(user=self.user, name="Vanilla")
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        ingredients = Ingredient.objects.all().order_by("-name")
+        serializer = IngredientSerializer(ingredients, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
