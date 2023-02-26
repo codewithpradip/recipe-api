e38f236e211a8802a22914301ea36ddff7dbbc74
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-02-26 15:20:25 +0545

    feat(recipe): implement image API to upload image into a recipe.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,8 +5,11 @@ Views for the recipe APIs
 from rest_framework import (
     viewsets,
     mixins,
+    status,
 )
 
+from rest_framework.decorators import action
+from rest_framework.respose import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -27,15 +30,29 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return self.queryset.filter(user=self.request.user).order_by("-id")
 
     def get_serializer_class(self):
-        """ "Return the serializer class for the request."""
+        """ Return the serializer class for the request."""
         if self.action == "list":
             return serializers.RecipeSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
     def perform_create(self, serializer):
         """create new recipe."""
         serializer.save(user=self.request.user)
+
+    @action(methods=["POST"], detail=True, url_path="upload-image")
+    def upload_image(self, request, pk=None):
+        """Upload an image to recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BaseRecipeAttrViewSet(
