c7ff2ca200d825b5e91299f2c762d6bf65f0db3f
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-02-26 15:24:55 +0545

    fix: Resolve Response import error in recipe views.py

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -9,7 +9,7 @@ from rest_framework import (
 )
 
 from rest_framework.decorators import action
-from rest_framework.respose import Response
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
