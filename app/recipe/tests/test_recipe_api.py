2ed11a6920b7d102fd142db0f52bf26f8d5ab648
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-02-27 15:47:00 +0545

    test(recipe): add test to filter recipes by ingredients.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -388,6 +388,26 @@ class PrivateRecipeApiTests(TestCase):
         self.assertIn(s2.data, res.data)
         self.assertNotIn(s3.data, res.data)
 
+    def test_filter_by_ingredients(self):
+        """Test filtering recipes by ingredients."""
+        r1 = create_recipe(user=self.user, title="Posh Beans on Toast")
+        r2 = create_recipe(user=self.user, title="Chicken Cacciatore")
+        in1 = Ingredient.objects.create(user=self.user, name="Feta Cheese")
+        in2 = Ingredient.objects.create(user=self.user, name="Chicken")
+        r1.ingredients.add(in1)
+        r2.ingredients.add(in2)
+        r3 = create_recipe(user=self.user, title="Red Lentil Daal")
+
+        params = {"ingredients": f"{in1.id},{in2.id}"}
+        res = self.client.get(RECIPES_URL, params)
+
+        s1 = RecipeSerializer(r1)
+        s2 = RecipeSerializer(r2)
+        s3 = RecipeSerializer(r3)
+        self.assertIn(s1.data, res.data)
+        self.assertIn(s2.data, res.data)
+        self.assertNotIn(s3.data, res.data)
+
 class ImageUploadTests(TestCase):
     """Tests for the image upload API."""
 
